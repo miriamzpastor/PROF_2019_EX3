89f27d8e72cf9e421c8a321b6cba5731d51498ab
miriamzpastor <miriam.zpastor@alumnos.upm.es>
2020-01-24 15:45:00 +0100

    Inicializamos Time con la fecha pedida

--- src/main/java/es/upm/grise/profundizacion2019/ex3/Time.java
+++ src/main/java/es/upm/grise/profundizacion2019/ex3/Time.java
@@ -5,9 +5,14 @@ import java.time.format.DateTimeFormatter;
 
 public class Time {
 	
-	public static String getFutureTime(long seconds) {
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
-		LocalDateTime time = LocalDateTime.now();
+	protected static DateTimeFormatter formatter;
+	protected static LocalDateTime time;
+	
+	protected static String getFutureTime(long seconds) {
+		formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"); 
+		//Se inicializa Time
+		LocalDateTime time = LocalDateTime.of(2020, 01, 01, 00,00, 00);
+	    time = LocalDateTime.now();
 		time.plusSeconds(seconds);
 		return formatter.format(time);	
 	}
