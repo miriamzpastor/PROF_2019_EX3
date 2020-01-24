bb0c6fbad4d8c8e582e0ccaca5ec5b6dc4e0bd6d
miriamzpastor <miriam.zpastor@alumnos.upm.es>
2020-01-24 15:46:47 +0100

    Se añade varibale Time y constructor vacio

--- src/main/java/es/upm/grise/profundizacion2019/ex3/MyClass.java
+++ src/main/java/es/upm/grise/profundizacion2019/ex3/MyClass.java
@@ -2,6 +2,11 @@ package es.upm.grise.profundizacion2019.ex3;
 
 public class MyClass {
 	
+    private Time hora=new Time();
+	
+	public MyClass() {
+		}
+	
 	public void nextTime(long seconds) {
 		String next = Time.getFutureTime(seconds);
 		System.out.println(next);
